fa43257498f7b626c34baa37f8cca0c8f448803f
Travis Mewborne <tmewborn@trinity.edu>
2019-01-31 11:54:20 -0600

    Minor formatting changes to help command in main and player classes

--- src/main/scala/mud/Main.scala
+++ src/main/scala/mud/Main.scala
@@ -5,10 +5,10 @@ import io.StdIn._
 object Main {
 	def main(args: Array[String]): Unit = {
 	  var input = readLine("Hello! What is your name?: ").trim.capitalize
-	  while (input == "") input = readLine("Please enter your name: ")
+	  while (input == "") input = readLine("Please enter your name: ").trim.capitalize
 	  val player1 = new Player(input, Room.rooms(0), List[Item](new Item("phone", "A device intended for communication, however typically used as a social deterrent.")))
 	  println("Hello " + input + "! Welcome to ___. Type \"help\" at any time for a list of possible commands.\nYou are currently in " + player1.currentLocation.getName + ", " + player1.currentLocation.description + "\n")
-	  println("Items in your " + player1.inventoryListing + "\n") //Print the items in the player's inventory.
+	  println("Item(s) in your " + player1.inventoryListing + "\n") //Print the items in the player's inventory.
 	  println(player1.currentLocation.itemList) //Print the items in the room
 	  println("\n" + player1.currentLocation.printExits) //Print the possible exits
 	  do {  
