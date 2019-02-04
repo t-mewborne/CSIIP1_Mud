f4c9e9a8b1a48af75ad852dc07fdb686b499626e
Mark Lewis <mlewis@trinity.edu>
2019-02-03 19:09:52 -0600

    Create ShowCodes.sh

---
+++ ShowCodes.sh
@@ -0,0 +1,5 @@
+## Show Code #1 on 1/31 (4 of 4 points)
+
+Compiled and ran. I felt that the way it ran was smooth and it took my commands the way I wanted to type them.
+I'm not big on printing both invetory and room items after every get/drop, but that's a design choice that you
+can keep if you like it.
