1f5f23a52178d25f81f8c8a21a150a3db6a36a28
Mark Lewis <mlewis@trinity.edu>
2019-02-24 09:11:16 -0600

    Show code #3.

--- ShowCodes.sh
+++ ShowCodes.sh
@@ -3,3 +3,9 @@
 Compiled and ran. I felt that the way it ran was smooth and it took my commands the way I wanted to type them.
 I'm not big on printing both invetory and room items after every get/drop, but that's a design choice that you
 can keep if you like it.
+
+## Show Code #3 on 2/19 (4 of 4 points)
+
+Code compiles and runs fine. Rooms are storedin a Map. You made exits be the Buffer, which seems like an odd 
+choice since you are never adding to or removing from the exits and that is the advantage of a Buffer. I also
+found a bug in item handling that I left two comments on.
